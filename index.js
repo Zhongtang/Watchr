bd28b03fadf462d70b9b8fe063415a13cdf99330
Zhongtang Tian <ztian@suremarket-lm.corp.yahoo.com>
2018-01-23 17:53:42 -0800

    input validation: apply regex; query IEX to verify if quote exists; force convert to lowercase

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
+const request = require('request')
+
 const app = express()
 
 const QUOTE_IIMIT = 5
 const PORT = 4080
+const QUOTE_REGEX =  /^[a-zA-Z0-9-.]{1,10}$/    // alphanumeric, dash, dot, 1 to 10 chars
+
+const IEX_API_BASE = 'https://api.iextrading.com/1.0/stock'
+
 var watching = {}
+
+// app.use(express.bodyParser())
+app.use((req, res, next) => {
+    logger(`${req.method} ${req.url}`)
+    next()
+})
+app.param('quote', (req, res, next, quoteParam) => {
+    if (!QUOTE_REGEX.test(quoteParam)) {
+        return res.status(400).send('Invalid quote ' + quoteParam)
+    }
+    req.params.quote = quoteParam.toLowerCase()
+    next()
+})
 
 app.get('/v1/stocks/', getAllQuotes)
 app.get('/v1/stocks/:quote/', getQuote)
 app.post('/v1/stocks/:quote/', addQuote)
 app.delete('/v1/stocks/:quote/', removeQuote)
-
 
 function getAllQuotes(req, res) {
     res.send(Object.keys(watching))
@@ -32,8 +50,17 @@ function addQuote(req, res) {
         return res.status(403).send('Number of watching stocks exceeded quota ' + QUOTE_IIMIT)
     }
 
-    watching[quoteName] = {}
-    res.status(204).send('Number of watching stocks exceeded quota ' + QUOTE_IIMIT)
+    var verifyUrl = getIexApiCompany(quoteName)
+    request.get(verifyUrl, (err, resp, body) => {
+        if (err) {
+            logger('[ERROR] get ${verifyUrl}: ' + err)
+        }
+        if (err || resp.statusCode < 200 || resp.statusCode >= 300) {
+            return res.status(404).send(`Quote ${quoteName} does't exist in our data source`)
+        }
+        watching[quoteName] = {}
+        res.status(204).send('Number of watching stocks exceeded quota ' + QUOTE_IIMIT)
+    })
 }
 
 function removeQuote(req, res) {
@@ -44,6 +71,15 @@ function removeQuote(req, res) {
 
     delete watching[quoteName]
     res.status(204).send()
+}
+
+function getIexApiCompany(quote) {
+    return `${IEX_API_BASE}/${quote}/company` 
+}
+
+function logger(content) {
+    var logTime = new Date().toISOString()
+    console.log(`${logTime} ${content}`)
 }
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT))
