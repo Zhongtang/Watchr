e9be80a973efc02e40f54498085904b2423c98dd
Zhongtang Tian <ztian@suremarket-lm.corp.yahoo.com>
2018-01-23 18:20:47 -0800

    rearrange files

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express')
-const request = require('request')
+
+const quotesMware = require('./lib/mware/quotesManage')
+const config = require('./lib/model/config')
 
 const app = express()
-
-const QUOTE_IIMIT = 5
-const PORT = 4080
-const QUOTE_REGEX =  /^[a-zA-Z0-9-.]{1,10}$/    // alphanumeric, dash, dot, 1 to 10 chars
-
-const IEX_API_BASE = 'https://api.iextrading.com/1.0/stock'
-
-var watching = {}
 
 // app.use(express.bodyParser())
 app.use((req, res, next) => {
@@ -17,69 +11,21 @@ app.use((req, res, next) => {
     next()
 })
 app.param('quote', (req, res, next, quoteParam) => {
-    if (!QUOTE_REGEX.test(quoteParam)) {
+    if (!config.QUOTE_FORMAT.test(quoteParam)) {
         return res.status(400).send('Invalid quote ' + quoteParam)
     }
     req.params.quote = quoteParam.toLowerCase()
     next()
 })
 
-app.get('/v1/stocks/', getAllQuotes)
-app.get('/v1/stocks/:quote/', getQuote)
-app.post('/v1/stocks/:quote/', addQuote)
-app.delete('/v1/stocks/:quote/', removeQuote)
-
-function getAllQuotes(req, res) {
-    res.send(Object.keys(watching))
-}
-
-function getQuote(req, res) {
-    var quoteName = req.params.quote
-    if (!watching[quoteName]) {
-        return res.status(404).send('The quote is not being watched')
-    }
-    res.status(204).send(watching[quoteName])
-}
-
-function addQuote(req, res) {
-    var quoteName = req.params.quote
-    if (watching[quoteName]) {
-        return res.status(204).send()
-    }
-    if (Object.keys(watching).length >= QUOTE_IIMIT) {
-        return res.status(403).send('Number of watching stocks exceeded quota ' + QUOTE_IIMIT)
-    }
-
-    var verifyUrl = getIexApiCompany(quoteName)
-    request.get(verifyUrl, (err, resp, body) => {
-        if (err) {
-            logger('[ERROR] get ${verifyUrl}: ' + err)
-        }
-        if (err || resp.statusCode < 200 || resp.statusCode >= 300) {
-            return res.status(404).send(`Quote ${quoteName} does't exist in our data source`)
-        }
-        watching[quoteName] = {}
-        res.status(204).send('Number of watching stocks exceeded quota ' + QUOTE_IIMIT)
-    })
-}
-
-function removeQuote(req, res) {
-    var quoteName = req.params.quote
-    if (!watching[quoteName]) {
-        return res.status(404).send('The quote is not being watched')
-    }
-
-    delete watching[quoteName]
-    res.status(204).send()
-}
-
-function getIexApiCompany(quote) {
-    return `${IEX_API_BASE}/${quote}/company` 
-}
+app.get('/v1/stocks/', quotesMware.getAllQuotes)
+app.get('/v1/stocks/:quote/', quotesMware.getQuote)
+app.post('/v1/stocks/:quote/', quotesMware.addQuote)
+app.delete('/v1/stocks/:quote/', quotesMware.removeQuote)
 
 function logger(content) {
     var logTime = new Date().toISOString()
     console.log(`${logTime} ${content}`)
 }
 
-app.listen(PORT, () => console.log('Listening on port ' + PORT))
+app.listen(config.PORT, () => console.log('Listening on port ' + config.PORT))
